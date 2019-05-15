59817b5075d7ad5d4a318c3afea7a59c2b69c548
cairuojin <137711786@qq.com>
2019-05-15 23:44:32 +0800

    添加父工程

--- SpringCloudTest_UserProvider/src/main/java/com/gjsyoung/SpringCloudTest_UserProvider/dao/UserRepository.java
+++ SpringCloudTest_UserProvider/src/main/java/com/gjsyoung/SpringCloudTest_UserProvider/dao/UserRepository.java
@@ -2,6 +2,8 @@ package com.gjsyoung.SpringCloudTest_UserProvider.dao;
 
 import com.gjsyoung.SpringCloudTest_UserProvider.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
