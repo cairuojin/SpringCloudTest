b43f111c944c8f590f76ec908c445eb4d151e69e
cairuojin <137711786@qq.com>
2019-05-16 16:32:41 +0800

    创建多个Eureka Server、

--- SpringCloudTest_UserProvider/src/main/java/com/gjsyoung/SpringCloudTest_UserProvider/dao/UserRepository.java
+++ SpringCloudTest_UserProvider/src/main/java/com/gjsyoung/SpringCloudTest_UserProvider/dao/UserRepository.java
@@ -4,6 +4,5 @@ import com.gjsyoung.SpringCloudTest_UserProvider.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
