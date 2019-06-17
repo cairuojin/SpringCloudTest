448c10da6dd1d418d6eca2c727c8e70584b7d055
cairuojin <137711786@qq.com>
2019-06-17 21:51:03 +0800

    hystrix

--- SpringCloudTest_MovieProvider_feign/src/main/java/com/gjsyoung/SpringCloudTest_MovieProvider/ProviderMovieApplication_feign.java
+++ SpringCloudTest_MovieProvider_feign/src/main/java/com/gjsyoung/SpringCloudTest_MovieProvider/ProviderMovieApplication_feign.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
 public class ProviderMovieApplication_feign {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderMovieApplication_feign.class, args);
 	}
-
 }
